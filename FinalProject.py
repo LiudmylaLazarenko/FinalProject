fada80a3163a2557b029db2b914cb6f135b273ce
Liudmyla Lazarenko <lazarenko.liudmyla@gmail.com>
2022-04-07 13:47:24 -0500

    FinalProject.py was modified

--- FinalProject.py
+++ FinalProject.py
@@ -10,15 +10,17 @@ logo = driver.find_element_by_xpath("//img[@alt='BotanikaBeauty']")
 #Twitter
 Twitter = driver.find_element_by_xpath("//a[@title='BotanikaBeauty on Twitter']")
 Twitter.click()
-
+driver.find_element_by_xpath("//*[@id]//h1/a").is_displayed
 
 #Facebook
 Facebook = driver.find_element_by_xpath("//a[@title='BotanikaBeauty on Facebook']")
 Facebook.click()
+driver.find_element_by_xpath("//*[@id]/div/div[1]/div/div[2]/div[1]/a").is_displayed
 
 #Instagram
 Instagram = driver.find_element_by_xpath("//a[@title='BotanikaBeauty on Instagram']")
 Instagram.click()
+driver.find_element_by_xpath("//img[@alt='Instagram']").is_displayed
 
 #Home
 Home = driver.find_element_by_xpath("//a[@title='Home']")
